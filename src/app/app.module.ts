05a42f4524e5f349df1699a89f3fa546dc485aec
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-04-15 16:12:22 +0300

    project setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from  '@angular/common/http';
+
+
+import{HttpServiceService} from './http-service.service'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ReposComponent } from './repos/repos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    ReposComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [HttpServiceService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
