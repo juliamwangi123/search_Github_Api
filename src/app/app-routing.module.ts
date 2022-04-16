1a577b929b61758bc0b253f7efa93b1335effac7
juliamwangi123 <julia.mwangi@student.moringaschool.com>
2022-04-17 01:45:21 +0300

    ft:add default path on load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import{ProfileComponent} from "./profile/profile.component";
+import{ProfileComponent} from "./profile/profile.component";
 import{AllReposComponent} from './all-repos/all-repos.component'
 
 const routes: Routes = [
-    // {path: "profile" ,component:ProfileComponent},
+    {path:"", redirectTo: "profile", pathMatch: "full"},
+    {path: "profile" ,component:ProfileComponent},
     {path: "allRepos" ,component:AllReposComponent},
 
 
